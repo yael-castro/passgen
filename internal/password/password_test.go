02850be54a9658102af424a20bf2c202e7980560
Yael Castro <yy.lgnd@gmail.com>
2021-11-29 15:14:58 -0600

    Refactored package password for add new features
    I create the package password where founds everything related to password
    generation. In the package founds the following:
    
    File "password.go" contains:
    
    * interface Generator implemented by all data types used to make
    passwords
    
    * struct Password used to generated passwords based on state and using
    non-repeat characters
    
    Tests for "password.go" are contained in the file "password_test.go" and it tests the following...
    
    * All features for the *password.Password* are tested in the function
    TestPassword (Have three sub-tests)
    
    issue #1

--- internal/password/password_test.go
+++ internal/password/password_test.go
@@ -6,43 +6,72 @@ import (
 	"testing"
 )
 
-type tableDrivenTesting struct {
-	Length      int
-	Letters     []rune
-	ExpectedSet [][]rune
+// tdtPassword is table driven testing for Password struct
+type tdtPassword struct {
+	expectedPasswords [][]rune
+	*Password
 }
 
 var (
-	tests = []tableDrivenTesting{
+	tests = []tdtPassword{
 		{
-			Length:  2,
-			Letters: []rune{'A', 'B'},
-			ExpectedSet: [][]rune{
-				{'B', 'B'},
-				{'B', 'A'},
-				{'A', 'B'},
-				{'A', 'A'},
+			expectedPasswords: [][]rune{
+				{'A', 'A', 'A'},
+				{'A', 'A', 'B'},
+				{'A', 'B', 'A'},
+				{'A', 'B', 'B'},
+				{'B', 'A', 'A'},
+				{'B', 'A', 'B'},
+				{'B', 'B', 'A'},
+				{'B', 'B', 'B'},
 			},
+			Password: &Password{
+				characters: []rune{'A', 'B'},
+				state:      []int{0, 0, 0},
+			},
+		},
+		{
+			expectedPasswords: [][]rune{
+				{'🐥', '🐣'},
+				{'🐥', '🐥'},
+			},
+			Password: &Password{
+				characters: []rune{'🐣', '🐥'},
+				state:      []int{1, 0},
+			},
+		},
+		{
+			Password: &Password{},
 		},
 	}
 
 	benchmark = tests[0]
 )
 
-func TestGenerateWordSet(t *testing.T) {
-	for i, test := range tests {
-		t.Run(strconv.Itoa(i+1), func(t *testing.T) {
-			gotSet := GenerateWordSet(test.Length, test.Letters)
+// TestPassword test password generation using the Password struct
+func TestPassword(t *testing.T) {
+	for i, v := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			for n, expected := range v.expectedPasswords {
+				if !v.Next() {
+					t.Fatal("could not generated more passwords ", n)
+				}
 
-			if !reflect.DeepEqual(gotSet, test.ExpectedSet) {
-				t.Errorf("expected '%v' got '%v'", test.ExpectedSet, gotSet)
+				got := v.Generate()
+
+				if !reflect.DeepEqual(expected, got) {
+					t.Errorf(`expected "%v" got "%v"`, expected, got)
+					continue
+				}
+
+				t.Log(string(got))
 			}
-		})
-	}
-}
 
-func BenchmarkGenerateWordSet(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		GenerateWordSet(benchmark.Length, benchmark.Letters)
+			if v.Next() {
+				t.Fatal("the Password creates more passwords than it should")
+			}
+
+			t.Log(v.State())
+		})
 	}
 }
